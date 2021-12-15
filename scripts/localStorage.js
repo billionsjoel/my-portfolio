17896acc66a63a64e3da0504dd4fa13ab0b3561e
Billions Joel <joelatugonza@gmail.com>
2021-12-15 16:43:23 +0300

    Fix deprecated value

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -5,28 +5,28 @@ const userMessage = form.elements['message'];
 /* ======= save the date in the js object ====== */
 
 form.addEventListener('change', () => {
-  let formObj = {};
+	let formObj = {};
 
-  if (userName.value.trim() !== '') {
-    formObj.name = userName.value.trim();
-  }
+	if (userName.value.trim() !== '') {
+		formObj.nameValue = userName.value.trim();
+	}
 
-  if (userEmail.value.trim() !== '') {
-    formObj.email = userEmail.value.trim();
-  }
+	if (userEmail.value.trim() !== '') {
+		formObj.email = userEmail.value.trim();
+	}
 
-  if (userMessage.value.trim() !== '') {
-    formObj.message = userMessage.value.trim();
-  }
+	if (userMessage.value.trim() !== '') {
+		formObj.message = userMessage.value.trim();
+	}
 
-  localStorage.setItem('formData', JSON.stringify(formObj));
+	localStorage.setItem('formData', JSON.stringify(formObj));
 });
 
 /* ======= loading the form data from the localStorage ====== */
 
 const localData = localStorage.getItem('formData');
-const { name, email, message } = localData ? JSON.parse(localData) : {};
+const { nameValue, email, message } = localData ? JSON.parse(localData) : {};
 
 userEmail.value = email ? email : '';
-userName.value = name ? name : '';
+userName.value = nameValue ? nameValue : '';
 userMessage.value = message ? message : '';
