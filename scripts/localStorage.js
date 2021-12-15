ddcda9ea22f49244db7ffa11f1b2e46858df92cf
Billions Joel <joelatugonza@gmail.com>
2021-12-15 16:47:39 +0300

    Fix linter error

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,25 +1,27 @@
-const userName = form.elements['userName'];
-const userEmail = form.elements['userEmail'];
-const userMessage = form.elements['message'];
+const formStorage = document.querySelector('.form-local-storage-class');
+
+const { userName } = formStorage.elements;
+const { userEmail } = formStorage.elements;
+const userMessage = formStorage.elements.message;
 
 /* ======= save the date in the js object ====== */
 
-form.addEventListener('change', () => {
-	let formObj = {};
+formStorage.addEventListener('change', () => {
+  const formObj = {};
 
-	if (userName.value.trim() !== '') {
-		formObj.nameValue = userName.value.trim();
-	}
+  if (userName.value.trim() !== '') {
+    formObj.nameValue = userName.value.trim();
+  }
 
-	if (userEmail.value.trim() !== '') {
-		formObj.email = userEmail.value.trim();
-	}
+  if (userEmail.value.trim() !== '') {
+    formObj.email = userEmail.value.trim();
+  }
 
-	if (userMessage.value.trim() !== '') {
-		formObj.message = userMessage.value.trim();
-	}
+  if (userMessage.value.trim() !== '') {
+    formObj.message = userMessage.value.trim();
+  }
 
-	localStorage.setItem('formData', JSON.stringify(formObj));
+  localStorage.setItem('formData', JSON.stringify(formObj));
 });
 
 /* ======= loading the form data from the localStorage ====== */
@@ -27,6 +29,6 @@ form.addEventListener('change', () => {
 const localData = localStorage.getItem('formData');
 const { nameValue, email, message } = localData ? JSON.parse(localData) : {};
 
-userEmail.value = email ? email : '';
-userName.value = nameValue ? nameValue : '';
-userMessage.value = message ? message : '';
+userEmail.value = email || '';
+userName.value = nameValue || '';
+userMessage.value = message || '';
