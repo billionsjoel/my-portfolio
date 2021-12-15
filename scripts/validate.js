38d39d15820381e179f4d467a2ad2fd73af8ac67
rachidelaid <rachidelaid@gmail.com>
2021-12-15 09:02:46 +0100

    Get form value and set constants

---
+++ scripts/validate.js
@@ -0,0 +1,6 @@
+/* ========= Get form values from UI ========== */
+const form = document.querySelector('.form');
+const NAME_REQUIRED = 'Please enter your name';
+const EMAIL_REQUIRED = 'Please enter your email';
+const MESSAGE_REQUIRED = 'Please enter your message';
+const EMAIL_INVALID = 'Please enter a correct email address format';
