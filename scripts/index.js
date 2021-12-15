c8113199d033a38758da927b6e77e604b7377725
Billions Joel <joelatugonza@gmail.com>
2021-12-15 05:07:15 +0300

    Fix merge conflict

--- scripts/index.js
+++ scripts/index.js
@@ -6,21 +6,12 @@ const about = document.querySelector('.about-link');
 const heroSection = document.querySelector('.portfolio-link');
 
 function displayMenu() {
-<<<<<<< HEAD
-	mobileNav.classList.add('show');
-	mobileNav.classList.remove('hide');
-}
-function closeMenu() {
-	mobileNav.classList.remove('show');
-	mobileNav.classList.add('hide');
-=======
   mobileNav.classList.add('show');
   mobileNav.classList.remove('hide');
 }
 function closeMenu() {
   mobileNav.classList.remove('show');
   mobileNav.classList.add('hide');
->>>>>>> 8b8b0bee2c813ea93ef92aad8e1b502125bdab9c
 }
 
 menuIcon.addEventListener('click', displayMenu, false);
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-let project = {
-	title: 'Multi-Post Stories',
-	description:
-		"A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
-	imageLink: 'about-me-img.png',
-	technologies: ['html', 'css', 'javascript'],
-	liveLink: 'https://live.com',
-	sourceLink: 'https://live.com',
-=======
 const project = {
   title: 'Multi-Post Stories',
   description: "A daily selection of privately personalized reads; no accounts or sign-ups required has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
@@ -15,7 +5,6 @@ const project = {
   technologies: ['html', 'css', 'javascript'],
   liveLink: 'https://billionsjoel.github.io/',
   sourceLink: 'https://github.com/billionsjoel/my-portfolio',
->>>>>>> 8b8b0bee2c813ea93ef92aad8e1b502125bdab9c
 };
 
 const grabBtnProject1 = document.querySelectorAll('.toggle-modal');
@@ -32,20 +21,6 @@ const techItem2 = document.querySelector('#item2');
 const techItem3 = document.querySelector('#item3');
 
 function showProject1() {
-<<<<<<< HEAD
-	popUp.classList.remove('hide');
-	popUp.classList.add('show');
-}
-function closeProject() {
-	popUp.classList.add('hide');
-}
-
-grabBtnProject1.forEach((e) => {
-	e.addEventListener('click', function (event) {
-		event.preventDefault();
-	});
-	e.addEventListener('click', showProject1, false);
-=======
   popUp.classList.remove('hide');
   popUp.classList.add('show');
 }
@@ -58,7 +33,6 @@ grabBtnProject1.forEach((e) => {
     event.preventDefault();
   });
   e.addEventListener('click', showProject1, false);
->>>>>>> 8b8b0bee2c813ea93ef92aad8e1b502125bdab9c
 });
 close.addEventListener('click', closeProject, false);
 
@@ -69,15 +43,9 @@ description.innerHTML = project.description;
 image.innerHTML = imgHtml;
 liveLink.href = project.liveLink;
 sourceLink.href = project.sourceLink;
-<<<<<<< HEAD
-techItem1.innerHTML = project.technologies[0];
-techItem2.innerHTML = project.technologies[1];
-techItem3.innerHTML = project.technologies[2];
-=======
 
 const [html, css, javascript] = project.technologies;
 
 techItem1.innerHTML = html;
 techItem2.innerHTML = css;
 techItem3.innerHTML = javascript;
->>>>>>> 8b8b0bee2c813ea93ef92aad8e1b502125bdab9c
