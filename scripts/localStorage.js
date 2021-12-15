ce953b0fa537f8458dfb96dd14fc3cd79408cade
rachidelaid <rachidelaid@gmail.com>
2021-12-15 14:18:21 +0100

    Adding the logic for loading form data from localStorage

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -5,10 +5,28 @@ const userMessage = form.elements['message'];
 /* ======= save the date in the js object ====== */
 
 form.addEventListener('change', () => {
-	let formObj = {
-		userName: userName.value,
-		userEmail: userEmail.value,
-		userMessage: userMessage.value,
-	};
-	localStorage.setItem('formData', JSON.stringify(formObj));
+  let formObj = {};
+
+  if (userName.value.trim() !== '') {
+    formObj.name = userName.value.trim();
+  }
+
+  if (userEmail.value.trim() !== '') {
+    formObj.email = userEmail.value.trim();
+  }
+
+  if (userMessage.value.trim() !== '') {
+    formObj.message = userMessage.value.trim();
+  }
+
+  localStorage.setItem('formData', JSON.stringify(formObj));
 });
+
+/* ======= loading the form data from the localStorage ====== */
+
+const localData = localStorage.getItem('formData');
+const { name, email, message } = localData ? JSON.parse(localData) : {};
+
+userEmail.value = email ? email : '';
+userName.value = name ? name : '';
+userMessage.value = message ? message : '';
